89a90e50dab67aad7798229558fbcb7a027b377f
Adam Bekuretsion <abekuretsion@box.com>
2018-01-14 12:09:49 -0800

    add animations

--- src/app.js
+++ src/app.js
@@ -1,82 +1,89 @@
 new Vue({
     el: '#app',
     data: {
-        playerHP: 100,
-        monsterHP: 100,
+        playerHealth: 100,
+        monsterHealth: 100,
         gameIsRunning: false,
-        turns: []
+        turns: [],
+        currentTurn: 0
     },
     methods: {
-        startGame: function() {
+        startGame: function () {
             this.gameIsRunning = true;
-            this.playerHP  = 100;
-            this.monsterHP = 100;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
             this.turns = [];
         },
-        attack: function() {
+        attack: function () {
             var damage = this.calculateDamage(3, 10);
-            this.monsterHP -= damage;
+            this.monsterHealth -= damage;
             this.turns.unshift({
                 isPlayer: true,
-                text: 'Player hits monster for ' + damage
+                text: 'Player hits Monster for ' + damage,
+                id: this.currentTurn + 1
             });
+            this.currentTurn++;
             if (this.checkWin()) {
                 return;
             }
 
             this.monsterAttacks();
         },
-        specialAttack: function() {
-            var damage =  this.calculateDamage(10, 20);
-            this.monsterHP -= damage;
+        specialAttack: function () {
+            var damage = this.calculateDamage(10, 20);
+            this.monsterHealth -= damage;
             this.turns.unshift({
                 isPlayer: true,
-                text: 'Player hits monster hard for ' + damage
+                text: 'Player hits Monster hard for ' + damage,
+                id: this.currentTurn + 1
             });
+            this.currentTurn++;
             if (this.checkWin()) {
                 return;
             }
-
             this.monsterAttacks();
         },
-        heal: function() {
-            if (this.playerHP <= 90) {
-                this.playerHP += 10;
+        heal: function () {
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10;
             } else {
-                this.playerHP = 100;
+                this.playerHealth = 100;
             }
             this.turns.unshift({
                 isPlayer: true,
-                text: 'Player heals for ' + 10
+                text: 'Player heals for 10',
+                id: this.currentTurn + 1
             });
+            this.currentTurn++;
             this.monsterAttacks();
         },
-        giveUp: function() {
+        giveUp: function () {
             this.gameIsRunning = false;
-            this.turns = [];
         },
         monsterAttacks: function() {
-            var damage = this.calculateDamage(5, 12)
-            this.playerHP -= damage;
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
             this.checkWin();
             this.turns.unshift({
                 isPlayer: false,
-                text: 'Monster hits player for ' + damage
+                text: 'Monster hits Player for ' + damage,
+                id: this.currentTurn + 1
             });
+            this.currentTurn++;
         },
         calculateDamage: function(min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
         checkWin: function() {
-            if (this.monsterHP <= 0) {
-                if (confirm('You won! New game?')) {
+            if (this.monsterHealth <= 0) {
+                if (confirm('You won! New Game?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
                 }
                 return true;
-            } else if (this.playerHP <= 0) {
-                if (confirm('You lost! New game?')) {
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')) {
                     this.startGame();
                 } else {
                     this.gameIsRunning = false;
